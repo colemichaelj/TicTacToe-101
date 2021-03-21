120e70b40ba2afd8835add611151cb783ec204ea
Michael Cole <colemichaelj@yahoo.com>
2021-03-21 14:27:29 -0500

    TicTacToe

--- scripts.js
+++ scripts.js
@@ -1,141 +1,176 @@
-//       ***********************
-//            INSTRUCTIONS
-//       ***********************
+'use strict';
 
-// 1. Read the code below one block at a time.
-// 2. Look for the @TODOs, and figure out how to fix them.
-    // next to each @TODO you will find tasks that need to be finished
-
-// The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
-let currentMarker = 'X'
+const assert = require('assert');
+const readline = require('readline');
+const { truncate } = require('fs');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
-  ]
+];
+
+let playerTurn = 'X';
 
 
-
-// this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
-// "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
-const handleClick = (element) => {
-
-  // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-  console.log(`The element you clicked on has an id:  ${element.id}`)
-
-  // this next line prevents an X being changed to an O or an O being changed to an X by...
-  //  checking to see if the square clicked has anything in it, if not continue
-  if(!document.getElementById(element.id).innerHTML){
-    addMarker(element.id)
-  }
+function printBoard() {
+  console.log('   0  1  2');
+  console.log('0 ' + board[0].join(' | '));
+  console.log('  ---------');
+  console.log('1 ' + board[1].join(' | '));
+  console.log('  ---------');
+  console.log('2 ' + board[2].join(' | '));
 }
-
-
-
-
-
-
-
-
-
-
-
-// this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
-const addMarker = (id) => {
-
-  // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
-  console.log(`*** The current marker is:  ${currentMarker}. ***`)
-  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
-  
-  // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
-  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-
-  
-  document.getElementById(id).innerHTML=currentMarker
-  
-
-  changeMarker()
-}
-
-
-
-
-
-
-
-
-
-
-// This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
-const changeMarker = () => {
-  if(currentMarker === "X"){
-    currentMarker = "O"
-  } else {
-    currentMarker = "X"
-  }}
-  const checkForWin = () => {
-    if(horizontalWin() || verticalWin() || diagonalWin()) {
-      window.alert(`Player ${currentMarker} won!`)
-    } else {
-      changeMarker()
-    }
+/*
+*if a player won horizontally on any row, return true
+*Otherwise return false
+*/
+function horizontalWin() {
+  if(board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X"){
+    return true
+  } else if (board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O"){
+    return true
+  } else if (board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X"){
+    return true
+  } else if(board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O"){
+    return true
+  } else if(board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X"){
+    return true
+  } else if(board[2][0] === "O" && board[2][1] === "O" && board[2][2] === "O"){
+    return true
+  } else{
+    return false
   }
-
-  const horizontalWin = () => {
-    // Your code here to check for horizontal wins
-    if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")){
-  return true}
-  else if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O"))
-  {return true}
-else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O"))
-{return true}}
-
-  const verticalWin = () => {
-    // Your code here to check for vertical wins
-    if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")){
-      return true}
-      else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O"))
-      {return true}
-    else if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O"))
-    {return true}}  
-  const diagonalWin = () => {
-    // Your code here to check for diagonal wins
-    if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")){
-      return true}
-      else if((board[2][0] == "X" && board[1][1] == "X" && board[0][2] == "X") || (board[2][0] == "O" && board[1][1] == "O" && board[0][2] == "O"))
-      {return true}}
-
-
-
-
-
-
-
-
-
-
-
-// This "resetBoard" function is called when the user clicks on the "Restart" button.
-const resetBoard= () => {
-  console.log("I")
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
     
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
+  }
+
+
+
 
 
   
-  const squares = document.getElementsByTagName("td")
   
+/*
+*if a player won vertically on any row, return true
+*Otherwise return false
+*
+*/
+function verticalWin() {
+  // Your code here
+  if(board[0][0] === "X" && board[1][0] === "X" && board[2][0] === "X"){
+    return true
+  } else if (board[0][0] === "O" && board[1][0] === "O" && board[2][0] === "O"){
+    return true
+  } else if (board[0][1] === "X" && board[1][1] === "X" && board[2][1] === "X"){
+    return true
+  } else if(board[0][1] === "O" && board[1][1] === "O" && board[2][1] === "O"){
+    return true
+  } else if(board[0][2] === "X" && board[1][2] === "X" && board[2][2] === "X"){
+    return true
+  } else if(board[0][2] === "O" && board[1][2] === "O" && board[2][2] === "O"){
+    return true
+  } else{
+    return false
+  }
+}
+/*
+*if a player won diagonally on any row, return true
+*Otherwise return false
+ */ 
+
+function diagonalWin() {
+  // Your code here
+  if(board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X"){
+    return true
+  } else if (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O"){
+    return true
+  } else if(board[0][2] === "X" && board[1][1] === "X" && board[0][2] === "X"){
+    return true
+  } else if (board[0][2] === "O" && board[1][1] === "O" && board[0][2] === "O"){
+    return true
+  } else {
+    return false
+  }
   
-  // loops over the HTML Collection of TDs and clears out the Xs and Os
-  for (i=0; i < squares.length; i++) {
+}
+// if they won H V D return true else return false
+function checkForWin() {
+  // code should call each check for types of wins
+  if(horizontalWin() == true || verticalWin() == true || diagonalWin == true ){
+    return true
+  } else {
+    return false
+  }
+}
 
-    // will log out the id of each square as it loops over them.
-    console.log(squares[i].id)
+function ticTacToe(row, column) {
+  // check that row and column are valid(optional)
+  // check that row and column are empty(optional)
 
-    // sets the innerHTML to null to replace the "X" or "O"
-    squares[i].innerHTML = null
-  }  
+  //update the board
+  board[row][column] = playerTurn;
+  if(playerTurn == "x") {
+    playerTurn = "O"
+  } 
+    checkForWin()
+  //
+  //check if they won? if they did print nice message, (optional)
+  //maybe clear board(optional)
+ // set the player turn to be the other player(still need to figure out how to swap)
+
+
+
+ //code here should set marker on the board(CHECK)
+ // then check for win
+}
+
+function getPrompt() {
+  printBoard();
+  console.log("It's Player " + playerTurn + "'s turn.");
+  rl.question('row: ', (row) => {
+    rl.question('column: ', (column) => {
+      ticTacToe(row, column);
+      getPrompt();
+    });
+  });
+
+}
+
+
+
+// Tests
+
+if (typeof describe === 'function') {
+
+  describe('#ticTacToe()', () => {
+    it('should place mark on the board', () => {
+      ticTacToe(1, 1);
+      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+    });
+    it('should alternate between players', () => {
+      ticTacToe(0, 0);
+      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+    });
+    it('should check for vertical wins', () => {
+      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
+      assert.equal(verticalWin(), true);
+    });
+    it('should check for horizontal wins', () => {
+      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
+      assert.equal(horizontalWin(), true);
+    });
+    it('should check for diagonal wins', () => {
+      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
+      assert.equal(diagonalWin(), true);
+    });
+    it('should detect a win', () => {
+      assert.equal(checkForWin(), true);
+    });
+  });
+} else {
+
+  getPrompt();
 }
